08b272311f310ee1e1ebd188f461e1f8dec3d7b5
dbjdbj <dbj@dbj.org>
2017-11-14 10:50:34 +0100

    dbj::print("dbj")("print")("is")("fluent");

--- dbj_console.h
+++ dbj_console.h
@@ -299,6 +299,7 @@ https://stackoverflow.com/questions/25885893/how-to-create-a-variadic-generic-la
 			if constexpr (sizeof...(param) > 0) {
 				char dummy[sizeof...(param)] = { (win::con::out( std::forward<decltype(param)>(param) ), 0)... };
 			}
+			return print;
 		};
 	}
 
