b4ae4731861da2773f2fc7a36280a87bea3765cd
dbjdbj <dbj@dbj.org>
2018-06-02 15:02:30 +0200

    better explained and presented

--- test/dbj_defval_testing.h
+++ test/dbj_defval_testing.h
@@ -2,55 +2,74 @@
 
 #ifdef	DBJ_TESTING_EXISTS
 namespace dbj_testing_space {
-	using namespace Gdiplus;
 
-	// test the const onstance behaviour
-	struct S final {
-		mutable dbj::holder<REAL> width{ 0 };
-		S(REAL def_width) : width(def_width) {}
-	};
+using namespace Gdiplus;
 
-	inline const S konst_{1024}; // initialized to 1024
+// test the const onstance behaviour
+struct S final {
+mutable dbj::holder<REAL> width{ 0 };
+S(REAL def_width) : width(def_width) {}
+};
 
-	inline dbj::holder<SmoothingMode> smoothnes
-		{ SmoothingMode::SmoothingModeAntiAlias };
+inline const S konst_{1024}; // initialized to 1024
 
-	inline dbj::holder<LineCap> linecap
-		{ LineCap::LineCapRound };
+inline dbj::holder<SmoothingMode> smoothnes
+{ SmoothingMode::SmoothingModeAntiAlias };
+
+inline dbj::holder<LineCap> linecap
+{ LineCap::LineCapRound };
+
+inline void test_dbj_defval_option_three() {
+// set the width to 512 for all the calls latter 
+auto	width_ = konst_.width(512);
+const	auto w1_ = konst_.width(); // 512
+auto	w2_ = konst_.width(); // 512
+
+_ASSERTE(width_ == 512);
+_ASSERTE(w2_ == w1_ && w2_ == width_ && w1_ == width_);
+
+//use the previosuly declared 
+// and instantiated def val holders
+auto lc_ = linecap();
+auto sness = smoothnes();
+}
 
 	DBJ_TEST_UNIT(" : dbj def val option three") {
-
-		// set the width to 512 for all the calls latter 
-		auto	width_		= konst_.width(512);
-		const	auto w1_	= konst_.width(); // 512
-		auto	w2_			= konst_.width(); // 512
-
-		_ASSERTE(width_ == 512);
-		_ASSERTE(w2_ == w1_ && w2_ == width_ && w1_ == width_);
-
-		//use the previosuly declared 
-		// and instantiated def val holders
-		auto lc_ = linecap();
-		auto sness = smoothnes();
+		// reminder: since C++11 local static vars
+		// are thread safe
+		static bool retval = [] { test_dbj_defval_option_three(); return true; }();
 	}
 
-	DBJ_TEST_UNIT(" : dbj def val option two") {
-		// create default values holders
-		// creation happens on first calls
-		// nice, but what is stopping us to do this 2 or more times?
-		// we will have 2 or more lambdas for each def val type
-		auto default_smoot = dbj::v_two::holder_maker(SmoothingMode::SmoothingModeAntiAlias);
-		auto default_lncap = dbj::v_two::holder_maker(LineCap::LineCapRound);
-		auto default_width = dbj::v_two::holder_maker(10);
+	/****************************************************************/
+	/*OPTION 2.1*/
+	/****************************************************************/
 
-		// just get the dflt width
-		auto dw0 = default_width();
-		auto dw1 = default_width();
-		// note: const ref is returned so we can 
-		// not assign (without stunts)  to ret value
-		// somewhere and sometimes latter someone 
-		// might change the default value
-		auto dw2 = default_width(42);
+using dbj::v_two::holder_maker;
+
+// inside named namespace
+// create default values holders
+// creation happens on first call
+inline auto default_smoot = 
+	dbj::v_two::holder_maker(SmoothingMode::SmoothingModeAntiAlias);
+
+inline auto default_lncap = 
+	dbj::v_two::holder_maker(LineCap::LineCapRound);
+
+inline auto default_width = 
+	dbj::v_two::holder_maker(10);
+
+inline void test_dbj_defval_option_two() {
+	// just get the dflt width
+	auto df0 = default_smoot();
+	auto df1 = default_lncap();
+
+	// somewhere and sometimes latter someone 
+	// might change the default value
+	auto df2 = default_width(42);
+}
+
+	DBJ_TEST_UNIT(" : dbj def val option two") {
+		static bool retval = [] { test_dbj_defval_option_two(); return true; }();
 	}
 }
 #endif
